9a8ee66c6098d5473eeba918ed5b8f7b6d247f43
panenosistema <juliaramosaraujo4@gmail.com>
2026-05-04 12:12:40 -0300

    adiciona o menu

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello word!')
+while True:
+    print('Sistema de Gestão de Biblioteca Comunitária')
+
+    print('1. Usuários')
+    print('2. Livros')
+    print('3. Empréstimos')
+    print('4. Sair')
+
+    opcao = input('Escolha a opção desejada: ')
