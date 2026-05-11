3cdfe5f0010a84e20afb706e97c1d949a8bd031b
Izabelly0 <00001126140922sp@al.educacao.sp.gov.br>
2026-05-11 09:24:21 -0300

    condições de menu

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 while True:
     print('Sistema de Gestão de Biblioteca Comunitária')
-
+    print ('''
+    ░█▀▄░▀█▀░█▀▄░█░░░▀█▀░█▀█░▀█▀░█▀▀░█▀▀░█▀█░░░░
+    ░█▀▄░░█░░█▀▄░█░░░░█░░█░█░░█░░█▀▀░█░░░█▀█░░░░
+    ░▀▀░░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀▀▀░▀░▀░░░░
+    ''')
     print('1. Usuários')
     print('2. Livros')
     print('3. Empréstimos')
     print('4. Sair')
 
     opcao = input('Escolha a opção desejada: ')
+    
+    if(opcao == '1'):
+        print('Usuários')
+    elif(opcao == '2'):
+        print('Livros')
+    elif(opcao == '3'):
+        print('Empréstimos')
+    elif(opcao == '4'):
+        break
+    else:
+        print('Opção inválida! Digite uma opção do menu...')
+
