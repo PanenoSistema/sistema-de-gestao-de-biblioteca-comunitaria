dc41aa80d45ced28b9cb1b87c6440d72281cf366
Izabelly0 <00001126140922sp@al.educacao.sp.gov.br>
2026-05-29 12:22:33 -0300

    alteração

--- main.py
+++ main.py
@@ -11,36 +11,39 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 # Função para exibir logotipo
-def show_tittle()
-
-# Função para exibir os menus
-def show_menu(menu):
-    clear_screen
-    if(menu == 'usuários')
-    clear_screen()
-        print('Usuários')
-        print('1. Novo usuário')
-        print('2. Ver usuários')
-        print('3. Voltar')
-while True:
+def show_tittle():
     print('Sistema de Gestão de Biblioteca Comunitária')
     print ('''
     ░█▀▄░▀█▀░█▀▄░█░░░▀█▀░█▀█░▀█▀░█▀▀░█▀▀░█▀█░░░░
     ░█▀▄░░█░░█▀▄░█░░░░█░░█░█░░█░░█▀▀░█░░░█▀█░░░░
     ░▀▀░░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀▀▀░▀░▀░░░░
     ''')
-    print('1. Usuários')
-    print('2. Livros')
-    print('3. Empréstimos')
-    print('4. Sair')
 
+# Função para exibir os menus
+def show_menu(menu):
+    clear_screen
+    if(menu == 'principal'):
+        print('1. Usuários')
+        print('2. Livros')
+        print('3. Empréstimos')
+        print('4. Sair')
+
+    if(menu == 'usuários'):    
+        print('Usuários')
+        print('1. Novo usuário')
+        print('2. Ver usuários')
+        print('3. Voltar')
+while True:
+    show_tittle()
+    show_menu('principal')
+    
     opcao = input('Escolha a opção desejada: ')
     
     if(opcao == '1'): # Opção  'Usuários' do menu principal
-        show_menu(menu)
+        show_menu('menu')
         opcao=input('Escolha uma opção: ')
         if opcao == 1: 
-            #cadastra um novo usuário
+            #adicionar
             pass
         elif opcao==2:
             #listar os usuários
